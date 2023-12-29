ecdc7dafdbe168bb6f1ea7b4bfe32448f3115d10
rilez <christopher.t.riley@gmail.com>
2023-12-29 11:42:18 -0800

    -- testing if need meta files

---
+++ Runtime/Scripts/Attributes/PriorityAttribute_Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UnityWindowsCapture.Runtime
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PriorityAttribute_Test : Attribute
+    {
+        public int Priority { get; set; }
+
+        public PriorityAttribute_Test(int priority)
+        {
+            Priority = priority;
+        }
+    }
+}
